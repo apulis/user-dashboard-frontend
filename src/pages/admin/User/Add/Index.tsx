9aacdfc025b7b89522117a850e3d68c90c51601d
Han <xjhan1995@gmail.com>
2020-03-30 18:28:50 +0800

    新增用户表单批量增加，删除，校验功能

--- src/pages/admin/User/Add/Index.tsx
+++ src/pages/admin/User/Add/Index.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from '@ant-design/compatible';
 import { Input, Button, Col, Row } from 'antd';
-import { FormComponentProps } from '@ant-design/compatible/es/form';
+import { FormComponentProps, ValidationRule } from '@ant-design/compatible/es/form';
 
 import styles from './index.less';
 
 const FormItem = Form.Item;
 
+interface UserMessage {
+  nickName: string;
+  userName: string;
+  phone?: string;
+  email?: string;
+  note?: string; 
+}
+
 
 const Add: React.FC<FormComponentProps> = props => {
-  const { form: {getFieldDecorator, validateFields } } = props;
+  const { form: { getFieldDecorator, validateFields, getFieldsValue } } = props;
+  const [userMessage, setUserMessage] = useState<UserMessage[]>([{
+    nickName: '',
+    userName: '',
+    phone: '',
+    email: '',
+    note: '',
+  }]);
   const formItemLayout = {
     labelCol: {
       xs: { span: 21 },
@@ -27,8 +42,28 @@ const Add: React.FC<FormComponentProps> = props => {
       }
     })
   };
-  const removeUser = () => {
-
+  const removeUser = (index: number) => {
+    //
+  }
+  const addUser = () => {
+    setUserMessage([...userMessage].concat({
+      nickName: '',
+      userName: '',
+      phone: '',
+      email: '',
+      note: '',
+    }))
+  }
+  const validateUserName= async (i: number, rule: any, value: any, callback: any) => {
+    const { userMessage } = getFieldsValue();
+    console.log(999, userMessage)
+    if (value) {
+      userMessage.forEach((user: UserMessage, index: number) => {
+        if (user.userName === value && i !== index) {
+          throw new Error('用户名需要唯一');
+        }
+      })
+    }
   }
   return (
     <div className={styles.add}>
@@ -49,41 +84,61 @@ const Add: React.FC<FormComponentProps> = props => {
           备注
         </Col>
       </Row>
-      <Row>
-        <Col span={4}>
-          <FormItem { ...formItemLayout }>
-            {getFieldDecorator('nickName', {
-              rules: [{ required: true, message: '需要创建昵称'}],
-            })(<Input placeholder="请输入昵称" />)}
-          </FormItem>
-        </Col>
-        <Col span={4}>
-          <FormItem { ...formItemLayout }>
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: '需要创建用户名'}],
-            })(<Input placeholder="请输入用户名" />)}
-          </FormItem>
-        </Col>
-        <Col span={4}>
-          <FormItem { ...formItemLayout }>
-            {getFieldDecorator('phone')(<Input placeholder="请输入手机" />)}
-          </FormItem>
-        </Col>
-        <Col span={4}>
-          <FormItem { ...formItemLayout }>
-            {getFieldDecorator('email')(<Input placeholder="请输入邮箱" />)}
-          </FormItem>
-        </Col>
-        <Col span={4}>
-          <FormItem { ...formItemLayout }>
-            {getFieldDecorator('note')(<Input placeholder="请输入备注" />)}
-          </FormItem>
-        </Col>
-        <Col span={2}><a onClick={() => removeUser() }>删除</a></Col>
-        
-      </Row>
-      <Button onClick={submit}>新增用户</Button><span>每次最多创建 10 个用户</span>
-      <Button type="primary">下一步</Button>
+      {
+        userMessage.map((_user, index) => (
+          <div>
+            <Row>
+              <Col span={4}>
+                <FormItem { ...formItemLayout }>
+                  {getFieldDecorator(`userMessage[${index}].nickName`, {
+                    initialValue: userMessage[index].nickName,
+                    rules: [{ required: true, message: '需要创建昵称'}],
+                  })(<Input placeholder="请输入昵称" />)}
+                </FormItem>
+              </Col>
+              <Col span={4}>
+                <FormItem { ...formItemLayout }>
+                  {getFieldDecorator(`userMessage[${index}].userName`, {
+                    initialValue: userMessage[index].userName,
+                    rules: [
+                      { required: true, message: '需要创建用户名'},
+                      { validator: (...args) => validateUserName(index, ...args) }
+                    ],
+                  })(<Input placeholder="请输入用户名" />)}
+                </FormItem>
+              </Col>
+              <Col span={4}>
+                <FormItem { ...formItemLayout }>
+                  {getFieldDecorator(`userMessage[${index}].phone`, {
+                    initialValue: userMessage[index].phone,
+                  })(<Input placeholder="请输入手机" />)}
+                </FormItem>
+              </Col>
+              <Col span={4}>
+                <FormItem { ...formItemLayout }>
+                  {getFieldDecorator(`userMessage[${index}].email`, {
+                    initialValue: userMessage[index].email,
+                  })(<Input placeholder="请输入邮箱" />)}
+                </FormItem>
+              </Col>
+              <Col span={4}>
+                <FormItem { ...formItemLayout }>
+                  {getFieldDecorator(`userMessage[${index}].note`, {
+                    initialValue: userMessage[index].note,
+                  })(<Input placeholder="请输入备注" />)}
+                </FormItem>
+              </Col>
+              <Col style={{marginTop: '8px'}} span={2}><a onClick={() => removeUser(index) }>删除</a></Col>
+            </Row>
+          </div>
+        ))
+      }
+      
+      
+      <Button onClick={addUser}>新增用户</Button><span>每次最多创建 10 个用户</span>
+      <div style={{marginTop: '40px'}}>
+        <Button onClick={submit} type="primary">下一步</Button>
+      </div>
     </div>
     
   );
