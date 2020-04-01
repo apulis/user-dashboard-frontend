dc9c3bf396f307744866e13f7e09a729d3c46e99
Han <xjhan1995@gmail.com>
2020-04-01 19:35:42 +0800

    修改 typo 错误

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,1 +1,1 @@
-export const emailRex =  /^[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}$/;
+export const emailReg =  /^[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}$/;
