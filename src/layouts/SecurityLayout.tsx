ab9051ec600a5ceb14d8576258c9ac0f85079bce
Han <xjhan1995@gmail.com>
2020-05-06 14:05:41 +0800

    收到回调token处理

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -5,6 +5,15 @@ import { Redirect } from 'umi';
 import { stringify } from 'querystring';
 import { ConnectState, ConnectProps } from '@/models/connect';
 import { CurrentUser } from '@/models/user';
+import { RouteComponentProps} from 'react-router-dom';
+import H from 'history';
+
+interface Location extends H.Location{
+  query: {[key: string]: string};
+}
+export interface PageProps extends RouteComponentProps {
+  location: Location;
+}
 
 interface SecurityLayoutProps extends ConnectProps {
   loading?: boolean;
@@ -15,21 +24,41 @@ interface SecurityLayoutState {
   isReady: boolean;
 }
 
-class SecurityLayout extends React.Component<SecurityLayoutProps, SecurityLayoutState> {
+class SecurityLayout extends React.Component<SecurityLayoutProps & SecurityLayoutState & PageProps> {
   state: SecurityLayoutState = {
     isReady: false,
   };
 
+  componentWillMount() {
+    let token = '';
+    const { dispatch, location, history } = this.props;
+    if (location && location.query && location.query.token) {
+      token = location.query.token;
+    }
+    console.log('tolen', token)
+    if (token) {
+      localStorage.token = token;
+      let redirectPath = location?.pathname;
+      const routerBase = window.routerBase;
+      if (routerBase.includes(redirectPath) || redirectPath?.includes(routerBase)) {
+        history && history.push('/');
+      } else {
+        history && history.push(location!.pathname);
+      }
+    }
+  }
   componentDidMount() {
     this.setState({
       isReady: true,
     });
-    const { dispatch } = this.props;
+    const { dispatch, location, history } = this.props;
     if (dispatch) {
       dispatch({
         type: 'user/fetchCurrent',
       });
+      
     }
+    
   }
 
   render() {
@@ -44,12 +73,12 @@ class SecurityLayout extends React.Component<SecurityLayoutProps, SecurityLayout
     if ((notLogin && loading) || !isReady) {
       return <PageLoading />;
     }
-    if (notLogin) {
-      return <Redirect to={`/user/login?${queryString}`}></Redirect>;
-    }
-    if (notRegister) {
-      return <Redirect to={`/user/register?${queryString}`}></Redirect>;
-    }
+    // if (notLogin) {
+    //   return <Redirect to={`/user/login?${queryString}`}></Redirect>;
+    // }
+    // if (notRegister) {
+    //   return <Redirect to={`/user/register?${queryString}`}></Redirect>;
+    // }
     return children;
   }
 }
