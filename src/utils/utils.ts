2d38b88b9615d09f95225473e6cd9be531b4bf6c
Han <xjhan1995@gmail.com>
2020-06-09 15:20:03 +0800

    增加 MD5 加密方法

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,7 @@
 import { parse } from 'querystring';
 import pathRegexp from 'path-to-regexp';
 import { Route } from '@/models/connect';
+import { Md5 } from "md5-typescript";
 
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
@@ -59,3 +60,8 @@ export const getRouteAuthority = (path: string, routeData: Route[]) => {
   });
   return authorities || '';
 };
+
+
+export function encodePassword(password: string) {
+  return Md5.init(password);
+}
