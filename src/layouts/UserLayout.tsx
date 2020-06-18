85c55118de4495cb779c2d784c95c6288197e6dd
Han <xjhan1995@gmail.com>
2020-06-18 15:31:20 +0800

    修改登录跳转链接是注册或登录页面时的bug

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,9 +1,9 @@
 import { MenuDataItem, getMenuData, getPageTitle } from '@ant-design/pro-layout';
 import { Helmet } from 'react-helmet';
 import { Link } from 'umi';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'dva';
-import { Row, Col } from 'antd';
+import { Row, Col, message } from 'antd';
 import { formatMessage } from 'umi-plugin-react/locale';
 
 import SelectLang from '@/components/SelectLang';
@@ -12,12 +12,13 @@ import styles from './UserLayout.less';
 import image1 from '@/assets/image1.jpeg';
 import image2 from '@/assets/image2.jpeg';
 import image3 from '@/assets/image3.jpeg';
+import { PageProps } from './SecurityLayout';
 
 export interface UserLayoutProps extends ConnectProps {
   breadcrumbNameMap: { [path: string]: MenuDataItem };
 }
 
-const UserLayout: React.FC<UserLayoutProps> = props => {
+const UserLayout: React.FC<UserLayoutProps & PageProps> = props => {
   const {
     route = {
       routes: [],
@@ -37,6 +38,31 @@ const UserLayout: React.FC<UserLayoutProps> = props => {
     formatMessage,
     ...props,
   });
+  useEffect(() => {
+    let token = '';
+    let error = '';
+    const { location, history } = props;
+    if (location && location.query && location.query.token) {
+      token = location.query.token;
+    }
+    if (token) {
+      localStorage.token = token;
+      history.push('/');
+    }
+    if (location && location.query && location.query.error) {
+      error = location.query.error;
+    }
+    if (error) {
+      message.error(error);
+      let redirectPath = location?.pathname;
+      const routerBase = window.routerBase;
+      if (routerBase.includes(redirectPath) || redirectPath?.includes(routerBase)) {
+        history && history.push('/');
+      } else {
+        history && history.push(location!.pathname);
+      }
+    }
+  }, [])
   return (
     <>
       <Helmet>
