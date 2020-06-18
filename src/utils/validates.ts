dd964468a1e9b84fd7a1f2baaea9befbe7aa02eb
Han <xjhan1995@gmail.com>
2020-06-18 16:59:42 +0800

    remove log

--- src/utils/validates.ts
+++ src/utils/validates.ts
@@ -5,7 +5,6 @@ import { IUserMessage } from "@/pages/admin/User/Add";
 export const emailReg =  /^[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}$/;
 
 export const validateUniqueUserName= async (i: number, data: any, _rule?: any, value?: any, callback?: any) => {
-  console.log('callback', callback)
   const userMessage = data;
   if (value) {
     userMessage.forEach((user: IUserMessage, index: number) => {
