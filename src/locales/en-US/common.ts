cd2fb770dc5fddb8fe543d632c61104d127efb71
hanxianjie100 <xianjiezh@gmail.com>
2020-07-31 23:59:13 +0800

    change plartform name

--- src/locales/en-US/common.ts
+++ src/locales/en-US/common.ts
@@ -1,5 +1,5 @@
 export default {
-  'common.platform.name': 'Apulis Platform',
+  'common.platform.name': 'Huawei Deep Learning Platform',
   'common.page.404.title': 'Sorry, the page you visited does not exist.',
   'common.page.404.button': 'Back Home',
   'common.page.403.title1': 'Sorry, you are not authorized to access this page.',
--- src/locales/zh-CN/common.ts
+++ src/locales/zh-CN/common.ts
@@ -1,5 +1,5 @@
 export default {
-  'common.platform.name': '华为 AI 平台',
+  'common.platform.name': '华为深度学习系统',
   'common.page.404.title': '访问的页面不存在',
   'common.page.404.button': '返回主页',
   'common.page.403.title1': '对不起，您没有当前页面的访问权限',
