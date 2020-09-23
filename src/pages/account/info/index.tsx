e8fa4da03ff4a3bd765b751d01b71ebcdcc5918a
JiangWei <Wei.Jiang@apulis.com>
2020-09-23 19:38:43 +0800

    完成微信和微软账号解绑功能

--- src/pages/account/info/index.tsx
+++ src/pages/account/info/index.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'dva';
 import { Descriptions, message } from 'antd';
 import { ConnectState, ConnectProps } from '@/models/connect';
+import { unbindMicrosoft, unbindWechat } from '@/services/unbind'
 
 const Info: React.FC<ConnectProps & ConnectState> = ({ user, dispatch, config }) => {
   const currentUser = user.currentUser!;
   const authMethods = config.authMethods;
   const { id } = currentUser;
+  // TODO 解绑之后，unbind之后fetchCurrent，并不有马上更新currentUser，而是必须得两次unbind之后currentUser的对应id才会置空。
+  console.log('xxxxxx')
+  console.log('render')
+  console.log(currentUser)
+  console.log('xxxxxx')
+  const [flag, setFlag] = useState(false)
+  useEffect(() => {},[flag])
   useEffect(() => {
     if (authMethods.length === 0) {
       dispatch({
@@ -24,6 +32,7 @@ const Info: React.FC<ConnectProps & ConnectState> = ({ user, dispatch, config })
       delete localStorage.bindType;
     }
   }, [currentUser])
+
   const bindMicrosoft = () => {
     localStorage.bindType = 'wechat';
     dispatch({
@@ -44,30 +53,57 @@ const Info: React.FC<ConnectProps & ConnectState> = ({ user, dispatch, config })
       }
     })
   }
+
+
+  const unBindMicrosoft = async () => {
+    const data = await unbindMicrosoft(id)
+    if (data.success) {
+      message.success(`Success unBind microsoft account!`);
+      dispatch({
+        type: 'user/fetchCurrent',
+      })
+      setFlag(!flag)
+    } else {
+      message.error(`Error unBind microsoft account!`);
+    }
+  }
+
+  const unBindWechat = async () => {
+    const data = await unbindWechat(id)
+    if (data.success) {
+      message.success(`Success unBind wechat account!`);
+      dispatch({
+        type: 'user/fetchCurrent',
+      })
+      setFlag(!flag)
+    } else {
+      message.error(`Error unBind wechat account!`);
+    }
+  }
   return (
     <>
       <Descriptions title="Account Info" layout="vertical" bordered>
-      <Descriptions.Item label="User Id">{currentUser.id}</Descriptions.Item>
+        <Descriptions.Item label="User Id">{currentUser.id}</Descriptions.Item>
         <Descriptions.Item label="Username">{currentUser.userName}</Descriptions.Item>
         <Descriptions.Item label="Nickname">{currentUser.nickName || '-'}</Descriptions.Item>
         <Descriptions.Item label="Current Role">{currentUser.currentRole.join(',')}</Descriptions.Item>
         <Descriptions.Item label="Phone">{currentUser.phone || '-'}</Descriptions.Item>
         <Descriptions.Item label="Email">{currentUser.email || '-'}</Descriptions.Item>
       </Descriptions>
-      <Descriptions title="Login Methods" layout="vertical" bordered style={{marginTop: '35px'}}>
+      <Descriptions title="Login Methods" layout="vertical" bordered style={{ marginTop: '35px' }}>
         {
           authMethods.includes('wechat') && <Descriptions.Item label="Wechat account">
-          {
-            currentUser.wechatId ? <div>Bound</div> : <><span>Not Bound</span><a onClick={bindWechat} style={{marginLeft: '15px'}}>To Bind</a></>
-          }
-        </Descriptions.Item>
+            {
+              currentUser.wechatId ? <><span>Bound status</span><a onClick={unBindWechat} style={{ marginLeft: '15px' }}>unBind</a></> : <><span>Not Bound status</span><a onClick={bindWechat} style={{ marginLeft: '15px' }}>To Bind</a></>
+            }
+          </Descriptions.Item>
         }
         {
           authMethods.includes('microsoft') && <Descriptions.Item label="Microsoft account">
-          {
-            currentUser.microsoftId ? <div>Bound</div> : <><span>Not Bound</span><a onClick={bindMicrosoft} style={{marginLeft: '15px'}}>To Bind</a></>
-          }
-        </Descriptions.Item>
+            {
+              currentUser.microsoftId ? <><span>Bound status</span><a onClick={unBindMicrosoft} style={{ marginLeft: '15px' }}>unBind</a></> : <><span>Not Bound status</span><a onClick={bindMicrosoft} style={{ marginLeft: '15px' }}>To Bind</a></>
+            }
+          </Descriptions.Item>
         }
       </Descriptions>
     </>
